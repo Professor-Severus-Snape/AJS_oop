ca958d8bb5509039dcb9284918e287cf0d85f1d0
Julia Fokanova <oncologmma@mail.ru>
2024-05-02 02:17:09 +0300

    change structure and paths

--- src/js/characters/__tests__/Character.test.js
+++ src/js/characters/__tests__/Character.test.js
@@ -1,23 +1,33 @@
 import Character from '../Character';
 
 test('testing of character creation without parameters', () => {
-  expect(() => { const character = new Character(); }).toThrowError('Ошибка в параметре name');
+  expect(() => {
+    const character = new Character();
+  }).toThrowError('Ошибка в параметре name');
 });
 
 test('testing of character creation with invalid name parameter (not a string)', () => {
-  expect(() => { const character = new Character(123); }).toThrowError('Ошибка в параметре name');
+  expect(() => {
+    const character = new Character(123);
+  }).toThrowError('Ошибка в параметре name');
 });
 
 test('testing of character creation with invalid name parameter (length < 2)', () => {
-  expect(() => { const character = new Character('л'); }).toThrowError('Ошибка в параметре name');
+  expect(() => {
+    const character = new Character('л');
+  }).toThrowError('Ошибка в параметре name');
 });
 
 test('testing of character creation with invalid name parameter (length > 10)', () => {
-  expect(() => { const character = new Character('лучник_лучник'); }).toThrowError('Ошибка в параметре name');
+  expect(() => {
+    const character = new Character('лучник_лучник');
+  }).toThrowError('Ошибка в параметре name');
 });
 
 test('testing of character creation with invalid type parameter', () => {
-  expect(() => { const character = new Character('не лучник', 'Not a Bowman'); }).toThrowError('Ошибка в параметре type');
+  expect(() => {
+    const character = new Character('не лучник', 'Not a Bowman');
+  }).toThrowError('Ошибка в параметре type');
 });
 
 test('testing levelUp method (health > 0)', () => {
@@ -25,7 +35,12 @@ test('testing levelUp method (health > 0)', () => {
   character.health = 25;
   character.levelUp();
   expect(character).toEqual({
-    name: 'лучник', type: 'Bowman', health: 100, level: 2, attack: 12, defence: 12,
+    name: 'лучник',
+    type: 'Bowman',
+    health: 100,
+    level: 2,
+    attack: 12,
+    defence: 12,
   });
 });
 
@@ -39,7 +54,12 @@ test('testing damage method (final health > 0)', () => {
   const character = new Character('лучник', 'Bowman');
   character.damage(33);
   expect(character).toEqual({
-    name: 'лучник', type: 'Bowman', health: 70, level: 1, attack: 10, defence: 10,
+    name: 'лучник',
+    type: 'Bowman',
+    health: 70,
+    level: 1,
+    attack: 10,
+    defence: 10,
   });
 });
 
@@ -47,6 +67,11 @@ test('testing damage method (final health < 0)', () => {
   const character = new Character('лучник', 'Bowman');
   character.damage(133);
   expect(character).toEqual({
-    name: 'лучник', type: 'Bowman', health: 0, level: 1, attack: 10, defence: 10,
+    name: 'лучник',
+    type: 'Bowman',
+    health: 0,
+    level: 1,
+    attack: 10,
+    defence: 10,
   });
 });
